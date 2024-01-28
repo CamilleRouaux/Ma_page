110a97308efd7d044682d35e929a0019eacd20e0
CamilleRouaux <rouaux.camille@hotmail.fr>
2024-01-28 23:45:56 +0100

    Working on css for the 'about' section

--- public/js/burger.js
+++ public/js/burger.js
@@ -8,7 +8,11 @@ burger.addEventListener('click', function(e){
 	document.querySelector('body').classList.toggle('overflow');
 })
 
-// body.addEventListener('click', function(){
-// 	document.querySelector('.nav-links').classList.remove("open");
-// 	document.querySelector('.burger').classList.remove("open");
-// })
+let hideMenu = document.querySelector('.nav-links');
+
+hideMenu.addEventListener('click', function(){
+	document.querySelector('.burger').classList.remove('clicked');
+	document.querySelector('.overlay').classList.remove('show');
+	document.querySelector('nav').classList.remove('show');
+	document.querySelector('body').classList.remove('overflow');
+})
