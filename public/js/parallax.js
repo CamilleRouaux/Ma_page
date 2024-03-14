b48cb2d82cb53e0924e1e9fc50666ff9bae608ca
CamilleRouaux <rouaux.camille@hotmail.fr>
2024-03-14 14:51:23 +0100

    Twinkling stars OK

--- public/js/parallax.js
+++ public/js/parallax.js
@@ -1,6 +1,4 @@
-let stars = document.getElementById('stars');
-// let moon = document.getElementById('moon');
-// let button = document.getElementById('button');
+let space = document.getElementById('space');
 let mountains_behind = document.getElementById('mountains_behind');
 let mountains_front = document.getElementById('mountains_front');
 let header = document.querySelector('header');
@@ -8,9 +6,7 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', function(){
 	let value = window.scrollY;
 	console.log(value);
-	stars.style.left = value * 0.25 + 'px';
-	// moon.style.top = (value == 0 ? 10 : value) * 1 + 'px';
+	space.style.left = value * 0.25 + 'px';
 	mountains_behind.style.top = value * 0.3 + 'px';
 	mountains_front.style.top = value * 0 + 'px';
-	// button.style.marginTop = value * 1.9 + 'px';
 })
